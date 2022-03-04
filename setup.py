38cc7f01fcb0c722a60a0a39f08087bb3dcf046b
Ektoplasma <acrasidae@gmail.com>
2022-03-04 15:14:58 +0100

    [ADD] Add setup files

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='iris_misp_module',
+    version='0.1',
+    packages=['iris_misp_module', 'iris_misp_module.misp_handler'],
+    url='https://github.com/dfir-iris/iris-misp-module',
+    license='LGPLv3.0',
+    author='ekt0, DFIR-IRIS',
+    author_email='contact@dfir-iris.org',
+    description='IRIS module interfacing MISP with IRIS',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: LGPLv3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "setuptools",
+        "celery~=4.4.7",
+        "pyunpack",
+        "iris_interface==1.1",
+        "pymisp~=2.4.152",
+        "Jinja2~=3.0.3"
+    ]
+)
